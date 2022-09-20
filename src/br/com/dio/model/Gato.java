b7d5fda2c3c5c4df7b27cf5dbce7b146d166581a
Jaqueline Agnes <agnesjaque@yahoo.com>
2022-09-20 20:47:53 -0300

    First Commit

---
+++ src/br/com/dio/model/Gato.java
@@ -0,0 +1,27 @@
+package br.com.dio.model;
+
+import java.util.Objects;
+
+public class Gato {
+
+	private String nome;
+	private String cor;
+	private Integer idade;
+
+	public Gato() {
+		this.nome = nome;
+		this.cor = cor;
+		this.idade = idade;
+		
+		
+	}
+
+	
+	}
+
+	/*
+	 * public Gato(String nome, String cor, Integer idade) this.cor = cor; this.nome
+	 * = nome; this.idade = idade ;
+	 */
+
+
